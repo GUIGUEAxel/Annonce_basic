dcc88ebba17bbe65dfd90dc8fe57f9397e27cad7
Kagumi <adrienjuttaud@gmail.com>
2026-03-27 11:48:13 +0100

    reset

---
+++ annonces_basic/View/post.php
@@ -0,0 +1,11 @@
+<?php $title= 'Exemple de post Basic PHP: Connexion'; ?>
+
+<?php ob_start(); ?>
+<h1><?php echo $post['title']; ?></h1>
+
+<div class="date"> <?php echo $post['date']; ?> </div>
+<div class="body"> <?php echo $post['body']; ?> </div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php'; ?>
