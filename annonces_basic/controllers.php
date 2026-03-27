1f2f645003da40b39de9f172704d858327f29bd7
GUIGUEAxel <125808598+GUIGUEAxel@users.noreply.github.com>
2026-03-27 12:27:54 +0100

    modif class controller

--- annonces_basic/controllers.php
+++ annonces_basic/controllers.php
@@ -1,23 +1,32 @@
 <?php
 
-function loginAction()
+class Controllers
 {
-    require 'view/login.php';
+    protected $data = null;
+
+    function __construct()
+    {
+        $this->data = $data;
+    }
+    public function loginAction()
+    {
+        require 'view/login.php';
+    }
+
+    public function annoncesAction($login, $password)
+    {
+        if (isUser($login, $password)) {
+            $annonces = getAllAnnonces();
+        } else {
+            $login = '';
+        }
+
+        require 'view/annonces.php';
+    }
+
+    public function postAction($id)
+    {
+        $post = getPost($id);
+        require 'view/post.php';
+    }
 }
-
-function annoncesAction($login, $password)
-{
-    if (isUser($login, $password))
-        $annonces = getAllAnnonces();
-    else
-        $login = '';
-
-    require 'view/annonces.php';
-}
-
-function postAction($id)
-{
-    $post = getPost($id);
-    require 'view/post.php';
-}
-
--- annonces_basic/model.php
+++ annonces_basic/model.php
@@ -1,4 +1,6 @@
 <?php
+
+
 function openConnection()
 {
     try {
