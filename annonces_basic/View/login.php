51101dc82c27066b9214c5d6f765667892817237
Kagumi <adrienjuttaud@gmail.com>
2026-03-27 12:12:25 +0100

    PDO

--- annonces_basic/View/login.php
+++ annonces_basic/View/login.php
@@ -3,10 +3,10 @@
 <?php ob_start(); ?>
     <form method="post" action="/annonces/index.php/annonces">
         <label for="login"> Votre identifiant </label> :
-        <input type="text" name="login" id="login" placeholder="defaut" maxlength="12" required />
+        <input type="text" name="login" id="login" placeholder="defaut" maxlength="12" value="admin"/>
         <br />
         <label for="password"> Votre mot de passe </label> :
-        <input type="password" name="password" id="password" maxlength="12" required />
+        <input type="password" name="password" id="password" maxlength="12" required value="admin" />
 
         <input type="submit" value="Envoyer">
     </form>
