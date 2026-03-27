ca66048035483f451d856a5108f05cfb907b5e28
GUIGUEAxel <125808598+GUIGUEAxel@users.noreply.github.com>
2026-03-27 10:57:47 +0100

    reglage des probleme de liens bdd
    encore des erreurs mais ça avances

--- annonces_basic/annonces.php
+++ annonces_basic/annonces.php
@@ -1,7 +1,7 @@
 <?php
-    $link = mysqli_connect('mysql-basic.alwaysdata.net', 'basic_annonces', '8DRhjvsdZZ2V8CG', 'basic_annonces_db');
+    $link = mysqli_connect('mysql-basic.alwaysdata.net', 'basic', '8DRhjvsdZZ2V8CG', 'basic_annonces_db');
     
-    $query= 'SELECT login FROM Users WHERE login="'.$_POST['login'].'" and password="'.$_POST['password'].'"';
+    $query= 'SELECT login FROM users WHERE login="'.$_POST['login'].'" and password="'.$_POST['password'].'"';
     $resultlogin = mysqli_query($link, $query );
     
     if( mysqli_num_rows( $resultlogin) ){
